28cff88f87006668c069e98816de86621558f71c
Your Name <your-email@example.com>
2026-06-14 11:30:47 -0700

    fix routes

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ MIDDLEWARE
 ========================
 */
 app.use(cors({
-  origin: ['https://studyai-richmond.netlify.app', 'http://localhost:5500', 'http://127.0.0.1:5500'],
+  origin: ['https://studyai-richmond.netlify.app', 
+          'http://localhost:5500',
+          'http://127.0.0.1:5500'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
---
+++ h
@@ -0,0 +1,5 @@
+[33m0c2a09e[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m fix ssl
+[33mfe261ef[m fix server.js
+[33mf9cb96a[m fix CORS
+[33m1181ac6[m fix CORS
+[33mb94e3bc[m fix API url
